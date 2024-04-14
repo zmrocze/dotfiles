a893f88c2fb6363a59cfda905e208164a364f17d
zmrocze <>
2024-04-14 22:39:43 +0200

    Note password

--- nixos/hosts/sda-omen.nix
+++ nixos/hosts/sda-omen.nix
@@ -1,3 +1,11 @@
+# KAROL:
+# so im using the old disk as backup
+# but actually decided to install something bootable there, why not
+
+# This is like omen-hardware.nix but different filesystems
+
+# Password set to "password"
+
 { mypkgs, ... }: {
   imports = [
     ../general.nix
